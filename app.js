9449b2a48c759a6ba5ac97833246e2d6a5fecfbe
Bruno Gui <hardy_gh@hotmail.com>
2023-01-31 12:11:00 -0500

    Conexion MDB + Archivos Estaticos

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 require('dotenv').config()
 
 const app = express()
 
+mongoose.set("strictQuery", false);
+
+mongoose.connect(`mongodb+srv://admindev:${process.env.MONGO_DB_PASS}@development.vcxzaiu.mongodb.net/control-stock-app?retryWrites=true&w=majority`)
+.then((result) => console.log('Conexion Exitosa a la BBDD'))
+.catch((err) => console.log(err))
+
+//Esquema de Mongo
+const productSchema = mongoose.Schema({
+    name: {type:String, required: true},
+    price: Number,
+},
+{
+    timestamps: true
+})
+
+//Modelo
+const Product = mongoose.model('Product', productSchema)
+
+
+app.use(express.json())
+
+app.post('/api/v1/products', (req, res, next) =>{
+
+    const newProduct = new Product(req.body)
+
+    newProduct.save().then( result =>{
+        res.status(201).json({ok: true})
+    })
+    .catch((err)=> console.log(err))
+    // console.log('Peticion recibida')
+    // console.log({body: req.body})
+    
+})
+
+
 //Archicos Estaticos
 app.use(express.static(path.join(__dirname, 'public')))
 
-//app.get('/',(req, res) =>{
-    console.log('Peticion recibida')
 
-    // res.status(200).sendFile('index.html',{ root: __dirname})
-//})
 
 const PORT = process.env.PORT 
 
--- public/index.js
+++ public/index.js
@@ -1,1 +1,21 @@
-console.log("ols")
+const inputName = document.querySelector('#productName')
+const inputPrice = document.querySelector('#productPrice')
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', (e) =>{
+    const name = inputName.value
+    const price = inputPrice.value
+
+    fetch('/api/v1/products', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            price,
+        }),
+    })
+})
+console.log({inputName, inputPrice})
